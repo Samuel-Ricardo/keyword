7b3ec89b98277871123baec189f196666ef5b906
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-12 10:58:12 -0300

    Create: Activity - BaseActivity (controller.base)

---
+++ app/src/main/java/com/study/kotlin/keyword/controller/base/BaseActivity.kt
@@ -0,0 +1,29 @@
+package com.study.kotlin.keyword.controller.base
+
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import com.study.kotlin.keyword.R
+import kotlinx.android.synthetic.main.example_list.*
+
+open class BaseActivity : AppCompatActivity() {
+
+    protected fun setupToolBar(toolbar: Toolbar, title:String, navigationBack: Boolean){
+        toolBar.title = title
+        setSupportActionBar(toolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(navigationBack)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+       when(item.itemId) {
+
+           R.id.home -> {
+               this.onBackPressed()
+               return true
+           }
+       }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
