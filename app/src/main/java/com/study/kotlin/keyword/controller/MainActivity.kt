6f0a3a4b6c796ae02e3e3405ebde1c5e388998a9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-12 09:44:46 -0300

    Create: Package - controller (com.study.kotlin)

--- app/src/main/java/com/study/kotlin/keyword/controller/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/keyword/controller/MainActivity.kt
@@ -1,12 +1,20 @@
-package com.study.kotlin.keyword
+package com.study.kotlin.keyword.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.study.kotlin.keyword.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun next_page(view: View) {
+
+        val intent:Intent = Intent(this)
     }
 }
