01286e1046425088ff6e5399115ca4b4d1f955b2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-13 12:09:49 -0300

    Create: Class - SQLite (storage)

--- app/src/main/java/com/study/kotlin/keyword/storage/SQLite.kt
+++ app/src/main/java/com/study/kotlin/keyword/storage/SQLite.kt
@@ -1,4 +1,34 @@
 package com.study.kotlin.keyword.storage
 
-class SQLite {
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+
+class SQLite(
+    context:Context
+): SQLiteOpenHelper(
+    context,
+    DATABASE_NAME,
+    null,
+    CURRENT_VERSION
+){
+    
+    companion object {
+        private val DATABASE_NAME = "keyword.db"
+        private val CURRENT_VERSION =  1
+    }
+
+    val TABLE_NAME = "key"
+    val COLUMNS_ID = "id"
+    val COLUMNS_NAME = "name"
+    val COLUMNS_LOGIN = "login"
+    val COLUMNS_PASSWORD = "password"
+    val DROP_TABLE = "DROP TABLE IF EXIST $TABLE_NAME"
+    val CREATE_TABLE = "CREATE TABLE $TABLE_NAME (" +
+            "$COLUMNS_ID INTEGER NOT NULL," +
+            "$COLUMNS_NAME TEXT NOT NULL," +
+            "$COLUMNS_LOGIN TEXT NOT NULL," +
+            "$COLUMNS_PASSWORD TEXT NOT NULL," +
+            "" +
+            "PRIMARY KEY($COLUMNS_ID AUTOINCREMENT)" +
+            ")"
 }
