8ec2a51d1f0f1cc91b215100b90504ff049c6039
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-13 20:14:06 -0300

    Create: Object - Columns (singleton.DatabaseConstants)

--- app/src/main/java/com/study/kotlin/keyword/singleton/DatabaseConstats.kt
+++ app/src/main/java/com/study/kotlin/keyword/singleton/DatabaseConstats.kt
@@ -4,19 +4,24 @@ object DatabaseConstats {
 
     val TABLE_NAME = "key"
 
-    val COLUMNS_ID = "id"
-    val COLUMNS_NAME = "name"
-    val COLUMNS_LOGIN = "login"
-    val COLUMNS_PASSWORD = "password"
+    val COLUMNS:Columns = Columns
 
     val SELECT_ALL = "SELECT * FROM $TABLE_NAME"
     val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
     val CREATE_TABLE = "CREATE TABLE $TABLE_NAME (" +
-            "$COLUMNS_ID INTEGER NOT NULL," +
-            "$COLUMNS_NAME TEXT NOT NULL," +
-            "$COLUMNS_LOGIN TEXT NOT NULL," +
-            "$COLUMNS_PASSWORD TEXT NOT NULL," +
+            "${COLUMNS.ID} INTEGER NOT NULL," +
+            "${COLUMNS.NAME} TEXT NOT NULL," +
+            "${COLUMNS.LOGIN} TEXT NOT NULL," +
+            "${COLUMNS.PASSWORD} TEXT NOT NULL," +
             "" +
-            "PRIMARY KEY($COLUMNS_ID AUTOINCREMENT)" +
+            "PRIMARY KEY(${COLUMNS.ID} AUTOINCREMENT)" +
             ")"
+}
+
+object Columns {
+
+    val ID = "id"
+    val NAME = "name"
+    val LOGIN = "login"
+    val PASSWORD = "password"
 }
