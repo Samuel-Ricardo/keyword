f2e5d614e692a812c14664289dbd52df7d2cbefa
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-13 18:52:55 -0300

    Create: Function - setupThis (controller.createKeyAction)

--- app/src/main/java/com/study/kotlin/keyword/controller/CreateKeyActivity.kt
+++ app/src/main/java/com/study/kotlin/keyword/controller/CreateKeyActivity.kt
@@ -1,13 +1,34 @@
 package com.study.kotlin.keyword.controller
 
 import android.os.Bundle
+import android.view.View
 import com.study.kotlin.keyword.R
 import com.study.kotlin.keyword.controller.base.BaseActivity
+import kotlinx.android.synthetic.main.create_key.*
 
 class CreateKeyActivity: BaseActivity() {
+
+    private var keyId:Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_key)
+        setupThis();
+    }
+
+    private fun setupThis(){
+
+        progressBar2.visibility = View.GONE
+
+    }
+
+    private fun saveKey(){
+
+        progressBar2.visibility = View.VISIBLE
+
+        val name = editTextName.text.toString()
+        val login = editTextLogin.text.toString()
+        val password = editTextPassword.text.toString()
+
     }
 }
