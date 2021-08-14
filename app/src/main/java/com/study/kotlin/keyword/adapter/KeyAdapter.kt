5dff3c38354b36f954699285e95611f824fdbef6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-14 00:06:22 -0300

    Create: Class - KeyAdapter (adapter)

--- app/src/main/java/com/study/kotlin/keyword/adapter/KeyAdapter.kt
+++ app/src/main/java/com/study/kotlin/keyword/adapter/KeyAdapter.kt
@@ -1,2 +1,13 @@
 package com.study.kotlin.keyword.adapter
 
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.study.kotlin.keyword.model.KeyVO
+
+class KeyAdapter(
+    private val context: Context,
+    private val keyList: List<KeyVO>,
+    private val onCLick: ((int) -> Unit)
+): RecyclerView.Adapter<KeyViewHolder>() {
+
+}
