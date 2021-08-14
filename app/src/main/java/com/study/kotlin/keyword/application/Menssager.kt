ab39c192aba477189839bdb30ac80683e6bd5fee
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-13 23:29:23 -0300

    Create: Function - showDialog (application.Menssager)

--- app/src/main/java/com/study/kotlin/keyword/application/Menssager.kt
+++ app/src/main/java/com/study/kotlin/keyword/application/Menssager.kt
@@ -1,13 +1,34 @@
 package com.study.kotlin.keyword.application
 
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.Toast
+
 class Menssager {
 
-    fun showMessage(
+    fun showDialog(
+        context:Context,
         title:String,
         message:String,
-        positiveButtonAction,
-        negativeButtonAction,
+        positiveButtonMessage:String,
+        negativeButton:String,
     ){
 
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(title)
+        builder.setMessage(message)
+
+        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+            Toast.makeText(context,
+                positiveButtonMessage, Toast.LENGTH_SHORT).show()
+        }
+
+        builder.setNegativeButton(android.R.string.no) { dialog, which ->
+            Toast.makeText(context,
+                negativeButton, Toast.LENGTH_SHORT).show()
+        }
+
+        builder.show();
     }
 }
