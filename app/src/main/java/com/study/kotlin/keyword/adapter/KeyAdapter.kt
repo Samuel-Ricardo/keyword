356795b879e683475d3c4ae86d72b9deda2ed673
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-14 00:12:25 -0300

    Update: Class - KeyAdapter (adapter)

--- app/src/main/java/com/study/kotlin/keyword/adapter/KeyAdapter.kt
+++ app/src/main/java/com/study/kotlin/keyword/adapter/KeyAdapter.kt
@@ -1,13 +1,26 @@
 package com.study.kotlin.keyword.adapter
 
 import android.content.Context
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.study.kotlin.keyword.model.KeyVO
 
 class KeyAdapter(
     private val context: Context,
     private val keyList: List<KeyVO>,
-    private val onCLick: ((int) -> Unit)
+    private val onCLick: ((Int) -> Unit)
 ): RecyclerView.Adapter<KeyViewHolder>() {
 
+    override fun onCreateViewHolder(paernt: ViewGroup, viewType: Int): KeyViewHolder {
+
+    }
+
+    override fun getItemCount(): Int = keyList.size
+
+    override fun onBindViewHolder(holder: KeyViewHolder, position: Int) {
+
+    }
 }
+
+class KeyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
