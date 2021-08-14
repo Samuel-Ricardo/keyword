91e3210b7392ca3db4411136f1379f5cbb9aa6b1
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-13 23:39:12 -0300

    Create: Function - showActionDialog (application.Menssager)

--- app/src/main/java/com/study/kotlin/keyword/application/Menssager.kt
+++ app/src/main/java/com/study/kotlin/keyword/application/Menssager.kt
@@ -3,6 +3,7 @@ package com.study.kotlin.keyword.application
 import android.app.AlertDialog
 import android.content.Context
 import android.widget.Toast
+import com.study.kotlin.keyword.interfaces.Function
 
 class Menssager {
 
@@ -11,10 +12,10 @@ class Menssager {
         title:String,
         message:String,
         positiveButtonMessage:String,
-        negativeButton:String,
+        negativeButtonMessage:String,
     ){
 
-        val builder = AlertDialog.Builder(this)
+        val builder = AlertDialog.Builder(context)
 
         builder.setTitle(title)
         builder.setMessage(message)
@@ -26,7 +27,31 @@ class Menssager {
 
         builder.setNegativeButton(android.R.string.no) { dialog, which ->
             Toast.makeText(context,
-                negativeButton, Toast.LENGTH_SHORT).show()
+                negativeButtonMessage, Toast.LENGTH_SHORT).show()
+        }
+
+        builder.show();
+    }
+
+    fun showActionDialog(
+        context:Context,
+        title:String,
+        message:String,
+        positiveButtonAction:Function,
+        negativeButtonAction:Function,
+    ){
+
+        val builder = AlertDialog.Builder(context)
+
+        builder.setTitle(title)
+        builder.setMessage(message)
+
+        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+            positiveButtonAction.run();
+        }
+
+        builder.setNegativeButton(android.R.string.no) { dialog, which ->
+            negativeButtonAction.run()
         }
 
         builder.show();
