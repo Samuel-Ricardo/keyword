86e319b1859da18c697d51fd4bc0e8d03a990616
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-15 10:44:45 -0300

    Setup: Activity - KeyListActivity (controller)

--- app/src/main/java/com/study/kotlin/keyword/controller/KeyListActivity.kt
+++ app/src/main/java/com/study/kotlin/keyword/controller/KeyListActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.study.kotlin.keyword.R
+import com.study.kotlin.keyword.adapter.KeyAdapter
 import com.study.kotlin.keyword.controller.base.BaseActivity
 import com.study.kotlin.keyword.model.KeyVO
 import kotlinx.android.synthetic.main.example_list.*
+import kotlinx.android.synthetic.main.key_list.*
 
 public class KeyListActivity: BaseActivity() {
+
+    private var adapter:KeyAdapter? = null;
 
     companion object {
         var selectedItem:KeyVO? = null
@@ -19,6 +24,15 @@ public class KeyListActivity: BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.key_list)
         setupToolBar(toolBar, "Lista de Chaves", false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        search()
+    }
+
+    private fun setup(){
+        recyclerView2.layoutManager = LinearLayoutManager(this)
     }
 
     fun goToCreate(view: View) {
