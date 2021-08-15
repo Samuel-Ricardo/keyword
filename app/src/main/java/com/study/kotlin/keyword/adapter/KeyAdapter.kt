afc509cc8bed073b3944ddc4524a5f620b9b79bd
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-15 10:32:33 -0300

    Update: Function - onCreateViewHolder (adapter.KeyAdapter)

--- app/src/main/java/com/study/kotlin/keyword/adapter/KeyAdapter.kt
+++ app/src/main/java/com/study/kotlin/keyword/adapter/KeyAdapter.kt
@@ -1,9 +1,11 @@
 package com.study.kotlin.keyword.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.study.kotlin.keyword.R
 import com.study.kotlin.keyword.model.KeyVO
 
 class KeyAdapter(
@@ -12,8 +14,10 @@ class KeyAdapter(
     private val onCLick: ((Int) -> Unit)
 ): RecyclerView.Adapter<KeyViewHolder>() {
 
-    override fun onCreateViewHolder(paernt: ViewGroup, viewType: Int): KeyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KeyViewHolder {
 
+        val view = LayoutInflater.from(context).inflate(R.layout.key_item, parent, false)
+        return KeyViewHolder(view)
     }
 
     override fun getItemCount(): Int = keyList.size
